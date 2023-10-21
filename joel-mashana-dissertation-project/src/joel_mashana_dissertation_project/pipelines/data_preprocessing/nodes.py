ae67eda1d48c2c88d5b6badd56eddd1370223cf2
JoelTMashana <joeltmashana@gmail.com>
2023-10-21 13:42:01 +0100

    organise nodes for pipeline

--- joel-mashana-dissertation-project/src/joel_mashana_dissertation_project/pipelines/data_preprocessing/nodes.py
+++ joel-mashana-dissertation-project/src/joel_mashana_dissertation_project/pipelines/data_preprocessing/nodes.py
@@ -1,8 +1,26 @@
-def split_data(data):
-    """splits data into two halves"""
-    midpoint = len(data) // 2
-    return data[:midpoint], data[midpoint:]
+import pandas as pd
 
-def sum_data(half_data):
-    """sums the data"""
-    return sum(half_data)
+def clean_payment_practise_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Your cleaning logic here
+    cleaned_data = data
+    return cleaned_data
+
+def clean_monthly_gdp_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Your cleaning logic here
+    cleaned_data = data
+    return cleaned_data
+
+def clean_inflation_rates_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Your cleaning logic here
+    cleaned_data = data
+    return cleaned_data
+
+def combine_datasets(payment_data: pd.DataFrame, gdp_data: pd.DataFrame, inflation_data: pd.DataFrame) -> pd.DataFrame:
+    # Your combining logic here
+    combined_data = payment_data  # Just a placeholder
+    return combined_data
+
+def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
+    # Your feature engineering logic here
+    feature_engineering_data = data
+    return feature_engineering_data
