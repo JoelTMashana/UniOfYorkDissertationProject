b926963eb25605e2b475f718c9c16cc8aea0fb24
JoelTMashana <joeltmashana@gmail.com>
2023-12-13 22:13:12 +0000

    test remove redundnat columns

--- joel-mashana-dissertation-project/src/tests/joel_mashana_dissertation_project/pipelines/nodes/test_data_preprocessing.py
+++ joel-mashana-dissertation-project/src/tests/joel_mashana_dissertation_project/pipelines/nodes/test_data_preprocessing.py
@@ -1,6 +1,8 @@
 import pytest
 import pandas as pd
-from joel_mashana_dissertation_project.pipelines.nodes.data_preprocessing import (filter_data_on_supplychain_finance, create_period_column)
+from joel_mashana_dissertation_project.pipelines.nodes.data_preprocessing import (filter_data_on_supplychain_finance, create_period_column, extract_payment_periods,
+                                                                                  remove_redundant_columns
+                                                                                  )
 
 #AAA
 
@@ -45,3 +47,47 @@ def test_create_period_column():
         '2021 JUN - 2021 JUL'
     ]) 
     assert result.columns[0] == 'Period'  
+
+
+def test_extract_payment_periods():
+    sample_data = {
+        'Start date': ['2016-06-01', '2017-04-01', '2018-11-20', '2019-05-15'],
+        'End date': ['2016-12-31', '2018-03-30', '2019-10-25', '2020-04-20']
+    }
+    df = pd.DataFrame(sample_data)
+
+ 
+    result = extract_payment_periods(df)
+
+    assert 2016 not in result.keys()
+    for year in range(2017, 2021):
+        assert year in result.keys()
+    
+    expected_periods = {
+        2017: ['(2017 APR, 2018 MAR)'],
+        2018: ['(2017 APR, 2018 MAR)', '(2018 NOV, 2019 OCT)'],
+        2019: ['(2018 NOV, 2019 OCT)', '(2019 MAY, 2020 APR)'],
+        2020: ['(2019 MAY, 2020 APR)']
+    }
+
+    assert result == expected_periods
+
+
+def test_remove_redundant_columns():
+    sample_data = {
+        'Start date': ['2021-01-01', '2021-02-01'],
+        'End date': ['2021-01-31', '2021-02-28'],
+        'Filing date': ['2021-02-15', '2021-03-15'],
+        'URL': ['https://check-payment-practices.service.gov.uk/report/2', 'https://check-payment-practices.service.gov.uk/report/3'],
+        'Shortest (or only) standard payment period': [30, 60]
+    }
+    df = pd.DataFrame(sample_data)
+
+ 
+    result = remove_redundant_columns(df)
+
+    assert 'Start date' not in result.columns
+    assert 'End date' not in result.columns
+    assert 'Filing date' not in result.columns
+    assert 'URL' not in result.columns
+    assert 'Shortest (or only) standard payment period' in result.columns
