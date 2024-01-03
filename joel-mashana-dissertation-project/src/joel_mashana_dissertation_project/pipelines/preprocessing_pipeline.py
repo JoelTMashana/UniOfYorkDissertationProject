d1646c3b0a122a1acaff2c7c5ac6299d7fa334e7
JoelTMashana <joeltmashana@gmail.com>
2024-01-03 09:17:20 +0000

    note on order

--- joel-mashana-dissertation-project/src/joel_mashana_dissertation_project/pipelines/preprocessing_pipeline.py
+++ joel-mashana-dissertation-project/src/joel_mashana_dissertation_project/pipelines/preprocessing_pipeline.py
@@ -808,6 +808,7 @@ def create_pipeline(**kwargs):
     return Pipeline(
         [ 
            ## Main pipeline
+           # Can be ran sequentially 
            filter_buyer_payment_practises_on_supply_chain_finance_node,
            create_period_column_node,
            extract_payment_periods_node,
@@ -818,7 +819,8 @@ def create_pipeline(**kwargs):
            determine_and_assign_risk_levels_buyer_data_node,
            mean_imputation_node,
            train_test_validate_split_node,
-           run_final_decision_tree_node,
+           # Must be run one by one, kedro does not allow you to run multiple nodes that save results to 'metrics'
+
         #    find_optimal_hyperparameter_ranges_for_decision_tree_node,
         #    find_optimal_hyperparameter_ranges_for_svm_node,
         #    find_optimal_hyperparameter_ranges_for_ann_node,
@@ -829,10 +831,12 @@ def create_pipeline(**kwargs):
             # find_best_hyperparameters_for_svm_node
             # svm_with_optimal_hyperparameters_node
             # decision_tree_with_optimal_hyperparameters_node
-
+            # run_final_decision_tree_node,
      
 
-
+        
+           # Can be ran sequentially 
+           
            ## Experimental nodes -- Buyer data only, smote applied and standard scaled various train test splits
 
         #    mean_imputation_for_experimental_data_node,
